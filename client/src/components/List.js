e507e04086ffd8529932e588f1fee7b92b4c1074
Claire Couppié <claire.couppie@gmail.com>
2020-04-12 12:26:58 +0200

    client:Add Product List

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from "graphql-tag";
+import ProductCard from './ProductCard';
+
+const GET_PRODUCTS = gql`
+{
+  products {
+    id
+    title
+    description
+  }
+}`
 
 export default () => {
-    return <div/>;
+  const { data, loading, error } = useQuery(GET_PRODUCTS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error</p>;
+  if (!data.products || !data.products.length) return <p>No product</p>;
+
+  return data.products.map((product) => {
+    return <ProductCard key={product.id} title={product.title} description={product.description} />;
+  });
 }
