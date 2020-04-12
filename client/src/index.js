575a77235552aaabd108ddfb8dac2ed9fe595f14
Claire Couppié <claire.couppie@gmail.com>
2020-04-12 12:09:03 +0200

    client:Setup ApolloClient to communicate with the server

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from '@apollo/react-hooks';
+import apolloClient from './middlewares/apolloClient';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={apolloClient}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
